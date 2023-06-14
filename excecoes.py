524402295bf76db350d2a32b2b06fed97b073a20
Paulo Palmeira <paulopalmeirasp@gmail.com>
2023-06-14 18:01:51 -0300

    update exceções

--- excecoes.py
+++ excecoes.py
@@ -4,11 +4,19 @@ try:
     numerador = int(input("Insira um número para dividir: "))
     denominador = int(input("Insira um número para ser divisor: "))
     resultado = numerador / denominador
-    print(resultado)
-except ZeroDivisionError:
+except ZeroDivisionError as e:
+    print(e)
     print("Não pode dividir por zero")
-except ValueError:
+except ValueError as e:
+    print(e)
     print("Digite apenas números")
+except Exception as e:
+    print(e)
+    print("Tem alguma coisa errada")
+else:
+    print(resultado)
+finally:
+    print("Isso aqui vai ser executado em qualquer dos casos")
 
 
 
